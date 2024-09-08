ed9742d35300c611b337def10ca947d4d5a2e376
Yashwanth P S <yashwanth.p.s@inorg.com>
2024-09-09 05:27:34 +0530

    Sample Spring Boot App

---
+++ src/test/java/com/harness/demo/controller/HelloControllerTests.java
@@ -0,0 +1,24 @@
+package com.harness.demo.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@WebMvcTest(HelloController.class)
+public class HelloControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testSayHello() throws Exception {
+        mockMvc.perform(get("/api/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello, World!"));
+    }
+}
